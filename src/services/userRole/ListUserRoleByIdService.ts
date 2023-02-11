c8d0a057f2093a8d371f95afd1eb8baa32003f1f
DanielSalis <danielsalis98@gmail.com>
2023-02-11 13:34:47 -0300

    feat: get userRole by id

---
+++ src/services/userRole/ListUserRoleByIdService.ts
@@ -0,0 +1,23 @@
+import prismaClient from '../../prisma';
+
+interface UserRoleRequest {
+  id: string
+}
+
+class ListUserRoleByIdService {
+  async execute({id}:UserRoleRequest){
+    if(!id) throw new Error('Provide the userRole ID');
+
+    const userRole = await prismaClient.userRole.findFirst({
+      where:{
+        id: id
+      }
+    });
+
+    if(!userRole) throw new Error('role not found');
+
+    return userRole;
+  }
+}
+
+export {ListUserRoleByIdService}
