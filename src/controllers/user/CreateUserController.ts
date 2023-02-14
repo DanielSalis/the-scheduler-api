329846dc8d5cb1ac800f09111279cf7631af443d
DanielSalis <danielsalis98@gmail.com>
2023-02-13 23:23:40 -0300

    feat: add create user controller

---
+++ src/controllers/user/CreateUserController.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from 'express';
+import { CreateUserService } from '../../services/user/CreateUserService';
+
+class CreateUserController {
+  async handle(req: Request, res: Response){
+    const { name, email, password, userRoleId, code } = req.body;
+
+    const userService = new CreateUserService();
+
+    const user = await userService.execute({
+      name,
+      email,
+      password,
+      userRoleId,
+      code
+    });
+
+    res.send(user);
+  }
+}
+
+export { CreateUserController };
