fe8282c6d98185ee1b766f431bd95b5c53e524fe
DanielSalis <danielsalis98@gmail.com>
2023-02-13 23:23:56 -0300

    feat: add create user service

---
+++ src/services/user/CreateUserService.ts
@@ -0,0 +1,47 @@
+import prismaClient from '../../prisma';
+import { hash } from 'bcryptjs';
+
+interface UserRequest{
+  name: string,
+  email: string,
+  password: string,
+  userRoleId: string,
+  code: string
+}
+
+class CreateUserService {
+  async execute(user: UserRequest){
+    if(!user.name || !user.password || !user.email){
+      throw new Error('Missing keys');
+    }
+
+    const userAlreadyExists = await prismaClient.user.findFirst({
+      where:{
+        email: user.email
+      }
+    });
+
+    if (userAlreadyExists) throw new Error('User already exists');
+
+    const passwordHashed = await hash(user.password, 8);
+
+    const userCreated = await prismaClient.user.create({
+      data: {
+        name: user.name,
+        email: user.email,
+        password: passwordHashed,
+        user_role_id: user.userRoleId,
+        code: user.code
+      },
+      select: {
+        id: true,
+        name: true,
+        email: true,
+      }
+    });
+
+    return userCreated;
+  }
+}
+
+export { CreateUserService };
