559c3da12d5e7ba53e760613ab8422e0b519f29c
DanielSalis <danielsalis98@gmail.com>
2023-03-15 16:04:11 -0300

    feat: create hospital controllers

---
+++ src/controllers/hospital/DeleteHospitalController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+import { DeleteHospitalByIdService } from '../../services/hospital/DeleteHospitalByIdService';
+
+class DeleteHospitalController {
+  async deleteById (req: Request, res: Response){
+    const { id } = req.params;
+
+    const service = new DeleteHospitalByIdService();
+
+    const hospital = await service.execute({
+      id
+    });
+
+    res.send(hospital);
+  }
+}
+
+export { DeleteHospitalController };
