cc6b997269b298095fd94382ecf929895775e48d
DanielSalis <danielsalis98@gmail.com>
2023-03-31 15:52:59 -0300

    fix: list all beds service return

--- src/services/bed/ListAllBedService.ts
+++ src/services/bed/ListAllBedService.ts
@@ -2,7 +2,14 @@ import prismaClient from '../../prisma';
 
 class ListAllBedService {
   async execute() {
-    const allBeds = await prismaClient.bed.findMany();
+    const allBeds = await prismaClient.bed.findMany({
+      select:{
+        id:true,
+        name:true,
+        unity: true,
+        classification:true
+      }
+    });
 
     if(!allBeds){
       throw('There is no bed created yet');
