2203efe8ce103841b388d5b28bae7ffe2cc5d9a2
DanielSalis <danielsalis98@gmail.com>
2023-06-17 10:06:16 -0300

    fix: syntax

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,6 +1,8 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /* eslint-disable no-undef */
-import { PrismaClient } from '@prisma/client';
-import { hash } from 'bcryptjs';
+const { PrismaClient } = require('@prisma/client');
+const { hash } = require('bcryptjs');
+
 const prisma = new PrismaClient();
 
 async function createUserRoles(){
