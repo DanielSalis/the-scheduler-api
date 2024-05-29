4b3f0e2104dc83a70e9cbe1cd6600d0c12939623
DanielSalis <danielsalis98@gmail.com>
2024-05-28 21:39:42 -0300

    feat: add prisma prod control

--- src/prisma/index.ts
+++ src/prisma/index.ts
@@ -1,5 +1,20 @@
 import { PrismaClient } from '@prisma/client';
 
-const prismaClient = new PrismaClient();
+declare global {
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient;
+}
 
-export default prismaClient;
+let prisma: PrismaClient;
+
+if (process.env.NODE_ENV === 'production') {
+    prisma = new PrismaClient();
+} else {
+    if (!global.prisma) {
+        global.prisma = new PrismaClient();
+    }
+
+    prisma = global.prisma;
+}
+
+export default prisma;
